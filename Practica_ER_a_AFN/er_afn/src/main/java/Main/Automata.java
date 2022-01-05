d3e7937e1b2ba66f07b69390a1622af61500ee0f
JesusSG88 <jsg_magicneko@hotmail.com>
2022-01-05 15:01:18 -0600

    add automata y estado

---
+++ Practica_ER_a_AFN/er_afn/src/main/java/Main/Automata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author matiassebastianparra
+ */
+public class Automata {
+
+    Estado inicio;
+    ArrayList<Estado> finales;
+    ArrayList<Estado> estados;
+    
+    public Automata() {        
+        finales = new ArrayList<Estado>();
+        estados = new ArrayList<Estado>();
+    }    
+
+    void setEstadoInicio(Estado inicio){
+        this.inicio = inicio;
+    }
+    
+    void agregarEstado(Estado estado){
+        this.estados.add(estado);
+    }
+    
+    void agregarEstadoFinal(Estado estado){
+        this.finales.add(estado);
+    }
+}
